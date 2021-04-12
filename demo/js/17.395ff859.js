db6ea194c0935f825c58c1d1b3acef007529c294
Dario Salvi <dariosalvi78@gmail.com>
2021-04-12 16:21:53 +0200

    updated demo

--- demo/js/17.395ff859.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[17],{"28e6":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},l=[],u={name:"EmptyPage"},c=u,r=t("2877"),s=Object(r["a"])(c,a,l,!1,null,null,null);n["default"]=s.exports}}]);
